ef6b1a69640f0a14af6fc682da9f01dce8d6d424
jeanse7en <linhquangdam@gmail.com>
2021-09-02 14:26:22 +0700

    Added Dsp

--- Source/PluginProcessor.cpp
+++ Source/PluginProcessor.cpp
@@ -93,8 +93,14 @@ void SimpleEqAudioProcessor::changeProgramName (int index, const juce::String& n
 //==============================================================================
 void SimpleEqAudioProcessor::prepareToPlay (double sampleRate, int samplesPerBlock)
 {
-    // Use this method as the place to do any pre-playback
-    // initialisation that you need..
+    juce::dsp::ProcessSpec spec;
+    spec.numChannels = 1;
+    spec.sampleRate = sampleRate;
+    spec.maximumBlockSize = samplesPerBlock;
+     
+    leftChain.prepare(spec);
+    rightChain.prepare(spec);
+    
 }
 
 void SimpleEqAudioProcessor::releaseResources()
@@ -144,18 +150,14 @@ void SimpleEqAudioProcessor::processBlock (juce::AudioBuffer<float>& buffer, juc
     for (auto i = totalNumInputChannels; i < totalNumOutputChannels; ++i)
         buffer.clear (i, 0, buffer.getNumSamples());
 
-    // This is the place where you'd normally do the guts of your plugin's
-    // audio processing...
-    // Make sure to reset the state if your inner loop is processing
-    // the samples and the outer loop is handling the channels.
-    // Alternatively, you can process the samples with the channels
-    // interleaved by keeping the same state.
-    for (int channel = 0; channel < totalNumInputChannels; ++channel)
-    {
-        auto* channelData = buffer.getWritePointer (channel);
-
-        // ..do something to the data...
-    }
+    juce::dsp::AudioBlock<float> block(buffer);
+    auto leftBlock = block.getSingleChannelBlock(0);
+    auto rightBlock = block.getSingleChannelBlock(1);
+    juce::dsp::ProcessContextReplacing<float> leftContext(leftBlock);
+    juce::dsp::ProcessContextReplacing<float> rightContext(rightBlock);
+    
+    leftChain.process(leftContext);
+    leftChain.process(rightContext);
 }
 
 //==============================================================================
